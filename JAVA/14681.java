b3ffaf35036972ae3aa7ea3c23a60fb130aa7a90
YejiJeonn <110978687+YejiJeonn@users.noreply.github.com>
2023-05-18 23:15:22 +0900

    Create 14681.java

---
+++ JAVA/14681.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        sc.close();
+
+        if(a > 0){
+            if(b > 0){
+                System.out.println("1");
+            }
+            else if(b < 0){
+                System.out.println("4");
+            }
+        }
+        else if(a < 0){
+            if(b > 0){
+                System.out.println("2");
+            }
+            else if(b < 0){
+                System.out.println("3");
+            }
+        }
+    }
+}
