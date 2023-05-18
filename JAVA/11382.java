9517f29f4d745275b0b6b678b5dedf82ba19c07a
YejiJeonn <110978687+YejiJeonn@users.noreply.github.com>
2023-05-18 23:14:46 +0900

    Create 11382.java

---
+++ JAVA/11382.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+
+        long a = sc.nextLong();
+        long b = sc.nextLong();
+        long c = sc.nextLong();
+
+        sc.close();
+
+        System.out.println(a+b+c);
+
+    }
+}
