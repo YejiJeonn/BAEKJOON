36ac20165b66f15b207ac01aeda86e1da2c7101a
YejiJeonn <110978687+YejiJeonn@users.noreply.github.com>
2023-05-18 23:13:53 +0900

    Create 10818.java

---
+++ JAVA/10818.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) throws Exception {
+    Scanner sc = new Scanner(System.in);
+
+    int n = sc.nextInt();
+    int[] arr = new int[n];
+    
+    for(int i=0; i<n; i++){
+      arr[i] = sc.nextInt();
+    }
+    
+    int min = arr[0], max = arr[0];
+
+    sc.close();
+
+    for(int i=0; i<n; i++){
+      if(max < arr[i]){
+        max = arr[i];
+      }
+
+      if(min > arr[i]){
+        min = arr[i];
+      }
+    }
+    System.out.println(min + " " + max);
+  }
+}
